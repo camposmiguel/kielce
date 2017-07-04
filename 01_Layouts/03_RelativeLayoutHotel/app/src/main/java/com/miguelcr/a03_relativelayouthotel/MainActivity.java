502984be22b6cd538910ab9646bb4572e58c46bf
Miguel Campos <camposmiguel@gmail.com>
2017-07-04 12:15:59 +0200

    Hotel image layout

---
+++ 01_Layouts/03_RelativeLayoutHotel/app/src/main/java/com/miguelcr/a03_relativelayouthotel/MainActivity.java
@@ -0,0 +1,36 @@
+package com.miguelcr.a03_relativelayouthotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import static com.miguelcr.a03_relativelayouthotel.R.id.imageView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewHotelName, textViewHotelPrice;
+    ImageView imageViewHotelPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Connect the visual components with the local variables
+        textViewHotelName = (TextView) findViewById(R.id.textViewName);
+        textViewHotelPrice = (TextView) findViewById(R.id.textViewPrice);
+        imageViewHotelPhoto = (ImageView) findViewById(R.id.imageViewPhoto);
+
+        // Change TextView text
+        textViewHotelName.setText("Kielce Cheapest Hotel!");
+        textViewHotelPrice.setText("€4");
+
+        // Load an image from Internet
+        Picasso.with(this)
+                .load("https://media-cdn.tripadvisor.com/media/photo-s/07/9b/68/0c/binkowski-hotel.jpg")
+                .into(imageViewHotelPhoto);
+
+    }
+}
