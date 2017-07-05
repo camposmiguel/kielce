448db5021c4ee8dffc740470ec61981b9414a24b
Miguel Campos <camposmiguel@gmail.com>
2017-07-05 12:31:49 +0200

    TicTacToe v4

--- 01_Layouts/04_TicTacToe/app/src/main/java/com/miguelcr/a04_tictactoe/GameActivity.java
+++ 01_Layouts/04_TicTacToe/app/src/main/java/com/miguelcr/a04_tictactoe/GameActivity.java
@@ -85,6 +85,7 @@ public class GameActivity extends AppCompatActivity {
             if (isPlayingPlayer1) {
                 cell.setImageResource(R.drawable.ic_player_1);
                 selectedCells[position] = 1;
+
                 isPlayingPlayer1 = false;
                 textViewPlayer.setText(player2Name);
             } else {
@@ -93,9 +94,83 @@ public class GameActivity extends AppCompatActivity {
                 isPlayingPlayer1 = true;
                 textViewPlayer.setText(player1Name);
             }
+
+            if(existSolution()) {
+
+            } else {
+
+            }
+
         } else {
             Toast.makeText(this, "The cell isn't empty!", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private boolean existSolution() {
+        boolean winner = false;
+
+        /*
+
+
+        0 1 2
+        3 4 5
+        6 7 8
+
+        Cells: 0, 4, 8 **********
+        x - -
+        - x -
+        - - x
+
+        Cells 2, 4, 6
+        - - x
+        - x -
+        x - -
+
+        Cells 0, 3, 6
+        x - -
+        x - -
+        x - -
+
+        Cells 1,4,7
+        - x -
+        - x -
+        - x -
+
+        Cells 2,5,8
+        - - x
+        - - x
+        - - x
+
+        Cells 0,1,2
+        x x x
+        - - -
+        - - -
+
+        Cells 3,4,5
+        - - -
+        x x x
+        - - -
+
+        Cells 6,7,8
+        - - -
+        - - -
+        x x x
+
+
+
+
+         */
+
+
+        if(selectedCells[0]!=0
+                && selectedCells[0] == selectedCells[4]
+                && selectedCells[4] == selectedCells[8]) {
+            winner = true;
+        } else if() {
+
+        }
+
+        return winner;
     }
 }
