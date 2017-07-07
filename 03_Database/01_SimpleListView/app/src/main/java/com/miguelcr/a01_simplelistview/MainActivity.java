7f8ab5ba7e673b87a8fdaf9321dcb4313868d4af
Miguel Campos <camposmiguel@gmail.com>
2017-07-07 10:50:12 +0200

    Database

--- 03_Database/01_SimpleListView/app/src/main/java/com/miguelcr/a01_simplelistview/MainActivity.java
+++ 03_Database/01_SimpleListView/app/src/main/java/com/miguelcr/a01_simplelistview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.miguelcr.a01_simplelistview;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,29 +10,41 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView lista;
-    List<String> students;
+    List<Student> students;
+    EditText editTextName;
+    Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // We initialize the database
+        initRealm();
+        realm = Realm.getDefaultInstance();
+
         // 1. Get the ListView reference to manage the list
         lista = (ListView) findViewById(R.id.listViewStudents);
 
         // 2. Create the list of data
-        students = new ArrayList<>();
+        // SELECT * FROM Student
+        // Give me the complete list of student from the database
+        students = realm.where(Student.class).findAll();
 
         // 3. Adapter (convert List<String> into ListView elements)
-        ArrayAdapter adapterStudents = new ArrayAdapter<String>(
+        StudentAdapter adapterStudents = new StudentAdapter(
                 this,
                 android.R.layout.simple_list_item_1,
                 students
@@ -43,11 +56,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // Item click event
         lista.setOnItemClickListener(this);
 
+
+    }
+
+    private void initRealm() {
+        Realm.init(getApplicationContext());
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .schemaVersion(1)
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String name = students.get(position);
+        String name = students.get(position).getName();
         Toast.makeText(this, "Student: "+name, Toast.LENGTH_SHORT).show();
 
         //view.setVisibility(View.GONE);
@@ -78,9 +101,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // 1. Instantiate an AlertDialog.Builder with its constructor
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
+        View view = getLayoutInflater().inflate(R.layout.dialog_new_student, null);
+        builder.setView(view);
+        editTextName = (EditText) view.findViewById(R.id.editTextNewStudent);
+
         // 2. Chain together various setter methods to set the dialog characteristics
         builder.setMessage(R.string.dialog_message)
                 .setTitle(R.string.dialog_title);
+
+        // Add the buttons
+        builder.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(MainActivity.this, "Name: "+editTextName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // If user click on cancel, we close the dialog
+                dialog.dismiss();
+            }
+        });
+
 
         // 3. Get the AlertDialog from create()
         AlertDialog dialog = builder.create();
---
+++ 03_Database/01_SimpleListView/app/src/main/java/com/miguelcr/a01_simplelistview/StudentAdapter.java
@@ -0,0 +1,46 @@
+package com.miguelcr.a01_simplelistview;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by miguelcampos on 6/7/17.
+ */
+
+public class StudentAdapter extends ArrayAdapter<Student> {
+    Context ctx;
+    int templateLayout;
+    List<Student> studentList;
+
+
+    public StudentAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Student> objects) {
+        super(context, resource, objects);
+
+        ctx = context;
+        templateLayout = resource;
+        studentList = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = LayoutInflater.from(ctx).inflate(templateLayout, parent, false);
+
+        // 1. Get the information for the current item
+        Student current = studentList.get(position);
+
+        // 2. Get all the view components references
+        TextView textViewName = (TextView) v.findViewById(R.id.textViewName);
+
+        return v;
+    }
+}
