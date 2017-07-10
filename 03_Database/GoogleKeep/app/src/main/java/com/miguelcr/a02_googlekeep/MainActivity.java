33bc4a452e7b7b29af7f019d83383b22b7f3c0e3
Miguel Campos <camposmiguel@gmail.com>
2017-07-10 10:48:19 +0200

    Google Keep v3

--- 03_Database/GoogleKeep/app/src/main/java/com/miguelcr/a02_googlekeep/MainActivity.java
+++ 03_Database/GoogleKeep/app/src/main/java/com/miguelcr/a02_googlekeep/MainActivity.java
@@ -1,9 +1,13 @@
 package com.miguelcr.a02_googlekeep;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -41,5 +45,49 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu_notes, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_new_note:
+                createNewNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void createNewNote() {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_new_note, null);
+        builder.setView(view);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_message_new_note)
+               .setTitle(R.string.dialog_title_new_note);
+
+        builder.setPositiveButton(R.string.save_new_note, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+
+
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+
+        // 4. show the dialog
+        dialog.show();
+
     }
 }
